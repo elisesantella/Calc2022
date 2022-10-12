d1ba6d12152a6f8d628708927eb170f2c9324deb
emilygavin <emilygavin@hotmail.com>
2022-10-12 10:04:00 +0100

    Rebased with Elise's branch

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -4,7 +4,25 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
         add();
+        subtract();
+    }
 
+    public static void subtract()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        int secondNumber = inputs.nextInt();
+
+        if(secondNumber > firstNumber){
+            System.out.println("Error: Second number is greater than first number.");
+        }
+        else{
+            int total = firstNumber - secondNumber;
+            System.out.println("The total is " + total);
+        }
     }
 
     public static void add(){
@@ -17,7 +35,5 @@ public class Calc {
 
         int total = valueOne + valueTwo;
         System.out.println("The total of the two values together is: " + total);
-
     }
-
 }
