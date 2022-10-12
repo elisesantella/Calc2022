08869f890bc0e37b290bf4d1ed08243ef9741000
emilygavin <emilygavin@hotmail.com>
2022-10-12 09:51:12 +0100

    Subtract method added

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,27 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        subtract();
+    }
 
+    public static void subtract()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        int secondNumber = inputs.nextInt();
+
+        if(secondNumber > firstNumber){
+            System.out.println("Error: Second number is greater than first number.");
+        }
+        else{
+            int total = firstNumber - secondNumber;
+            System.out.println("The total is " + total);
+        }
     }
 }
