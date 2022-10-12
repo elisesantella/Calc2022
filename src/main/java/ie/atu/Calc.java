3bc5d1341fab7c61e558be286f4f3b1357fab3e2
CLIODHNA NI NIAIDH - STUDENT <G00386395@atu.ie>
2022-10-12 09:45:06 +0100

    created multiply function

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,20 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        multiply();
+    }
+    public static void multiply(){
+        System.out.println("Please enter first number: ");
+        Scanner input = new Scanner(System.in);
+        int firstNum = input.nextInt();
 
+        System.out.println("Please enter second number: ");
+        int secondNum = input.nextInt();
+
+        int total = firstNum*secondNum;
+        System.out.println("Total: "+total);
     }
 }
